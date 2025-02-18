4b0572ef0e8783706a53a97cc77d24330fb484d3
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-02-19 01:28:29 +0700

    Replace @app.before_first_request with app context for table creation

--- CodeTrek18.py
+++ CodeTrek18.py
@@ -14,13 +14,11 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 @app.route ('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all
     app.run(debug = True)
