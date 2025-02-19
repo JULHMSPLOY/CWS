86c7c62aba1eb999ddcf9174aa682ed26f3bdbce
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-02-19 16:33:24 +0700

    Import generate_password_hash from werkzeug and hash password using sha256

--- CodeTrek18.py
+++ CodeTrek18.py
@@ -5,32 +5,37 @@ from flask import redirect
 from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from werkzeug.security import generate_password_hash
 
-app = Flask(__name__)
+app = Flask(__name__)  
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-Bootstrap(app) 
+Bootstrap(app)
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(120), unique = True, nullable = False)
-    email = db.Column(db.String(120), unique = True, nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
-    
-@app.route('/signup', methods = ['GET', 'POST'])
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
+        password = request.form['password']
 
         user_exists = User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first()
         if user_exists:
             return "Username or Email already exists!"
-    
-        new_user = User(username=username, email=email)
+
+        hashed_password = generate_password_hash(password, method='sha256')
+
+        new_user = User(username=username, email=email, password=hashed_password)
 
         db.session.add(new_user)
         db.session.commit()
@@ -39,10 +44,10 @@ def signup():
 
     return render_template('signup.html')
 
-
-@app.route ('/')
+@app.route('/')
 def home():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
