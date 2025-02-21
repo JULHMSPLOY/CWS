a596cd284364deff3170ace5c0030235a969dcae
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-02-21 21:12:39 +0700

    Handle POST request and extract user code and hint option

--- CodeTrek18.py
+++ CodeTrek18.py
@@ -293,6 +293,10 @@ def sql_practice():
     next_challenge = None
     test_status = None
 
+    if request.method == 'POST':
+        user_code = request.form['code']
+        show_hint = request.form.get('show_hint', False)
+
 
 if __name__ == '__main__':
     with app.app_context():
