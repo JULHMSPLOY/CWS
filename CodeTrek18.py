5799b6574c296283285986a2169f048128979a1b
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-02-22 14:13:57 +0700

    Add first_name column to database

--- CodeTrek18.py
+++ CodeTrek18.py
@@ -28,6 +28,7 @@ class User(db.Model):
     username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), unique=True, nullable=False)
+    first_name = db.Column(db.String(120), nullable=True)
 
     def __repr__(self):
         return f'<User {self.username}>'
