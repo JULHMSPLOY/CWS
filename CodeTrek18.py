ac0e763309cb4a4431a0b6db19b85f332ef04ae3
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-02-23 17:15:55 +0700

    Add one-to-one relationship between User and UserSettings models

--- CodeTrek18.py
+++ CodeTrek18.py
@@ -79,6 +79,7 @@ class User(db.Model):
     profile_picture = db.Column(db.String(120), nullable=True)
     progress = db.relationship('UserProgress', backref='user', lazy=True)
     attempts = db.relationship('UserAttempt', backref='user', lazy=True)
+    settings = db.relationship('UserSettings', backref='user', uselist=False)
 
     def __repr__(self):
         return f'<User {self.username}>'
